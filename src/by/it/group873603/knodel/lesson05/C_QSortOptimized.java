b4a49097b36d72800f395ca05edfed4efc994002
ExxZzyy <66119764+ExxZzyy@users.noreply.github.com>
2021-03-22 14:50:42 +0300

    Lesson5(A,B,C)

--- src/by/it/group873603/knodel/lesson05/C_QSortOptimized.java
+++ src/by/it/group873603/knodel/lesson05/C_QSortOptimized.java
@@ -44,9 +44,109 @@ public class C_QSortOptimized {
 
         @Override
         public int compareTo(Object o) {
-            //подумайте, что должен возвращать компаратор отрезков
-            return 0;
+            if (o instanceof Integer) {
+                return Integer.compare(this.start, (int) o);
+            }
+            return -1;
         }
+    }
+
+    void sort(Segment[] input, int left, int right) {
+        if (right > left) {
+            return;
+        }
+
+        if (input[left].compareTo(input[right]) > 0) {
+            swap(input, left, right);
+        }
+
+        Segment p = input[left];
+        Segment q = input[right];
+
+        int lt = left + 1;
+        int gt = right - 1;
+        int k = lt;
+
+        while (k <= gt) {
+            if (input[k].compareTo(p) < 0) {
+                swap(input, k, lt++);
+            }
+            else if (input[k].compareTo(q) >= 0) {
+                while (input[gt].compareTo(q) > 0 && k < gt) {
+                    --gt;
+                }
+                swap(input, k, gt--);
+
+                if (input[k].compareTo(p) < 0) {
+                    swap(input, k, lt++);
+                }
+            }
+            ++k;
+        }
+        lt--;
+        gt++;
+
+        swap(input, left, lt);
+        swap(input, right, gt);
+
+        sort(input, left, --lt);
+        sort(input, ++lt, --gt);
+        sort(input, ++gt, right);
+    }
+
+    private void swap(Segment[] input, int i, int j) {
+        Segment temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+
+    int count(Segment[] input, int value, int length) {
+        int i = 0;
+        int j = 0;
+
+        i = first(input, 0, length - 1, value, length);
+
+        if(i == -1) {
+            return i;
+        }
+
+        j = last(input, i, length - 1, value, length);
+
+        return j - i + 1;
+    }
+
+    int first(Segment[] input, int low, int high, int value, int length) {
+        if(high >= low) {
+            int mid = (low + high) % 2 == 0 ? (low + high) / 2 : ((low + high) / 2) + 1;
+
+            if((mid == 0 || value > input[mid - 1].start && value > input[mid - 1].stop) && (value >= input[mid].start && value <= input[mid].stop)) {
+                return mid;
+            }
+            else if(value > input[mid].start && value > input[mid].stop) {
+                return first(input, (mid + 1), high, value, length);
+            }
+            else {
+                return first(input, low, (mid - 1), value, length);
+            }
+        }
+        return -1;
+    }
+
+    int last(Segment[] input, int low, int high, int value, int length) {
+        if(high >= low) {
+            int mid = (low + high) % 2 == 0 ? (low + high) / 2 : ((low + high) / 2) + 1;
+
+            if((mid == length - 1 || value < input[mid + 1].start && value < input[mid + 1].stop) && (value >= input[mid].start && value <= input[mid].stop)) {
+                return mid;
+            }
+            else if(value < input[mid].start && value < input[mid].stop) {
+                return last(input, low, (mid - 1), value, length);
+            }
+            else {
+                return last(input, (mid + 1), high, value, length);
+            }
+        }
+        return -1;
     }
 
 
@@ -68,12 +168,18 @@ public class C_QSortOptimized {
             segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
         }
         //читаем точки
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             points[i]=scanner.nextInt();
         }
         //тут реализуйте логику задачи с применением быстрой сортировки
         //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
 
+        sort(segments, 0, segments.length - 1);
+
+        for (int i = 0; i < points.length; i++) {
+            int count = count(segments, points[i], segments.length);
+            result[i] = count == -1 ? 0 : count;
+        }
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
