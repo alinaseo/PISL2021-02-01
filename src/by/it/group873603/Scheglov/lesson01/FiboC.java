73d2e1aea3945a05113cd1b4d1bcdf7b14f5f327
HIRURGOSIN <44680348+HIRURGOSIN@users.noreply.github.com>
2021-04-05 14:25:49 +0300

    les4,5(a.b)

--- src/by/it/group873603/Scheglov/lesson01/FiboC.java
+++ src/by/it/group873603/Scheglov/lesson01/FiboC.java
@@ -6,6 +6,8 @@ package by.it.group873603.Scheglov.lesson01;
  * время расчета должно быть не более 2 секунд
  */
 
+import java.util.ArrayList;
+
 public class FiboC {
 
     private long startTime = System.currentTimeMillis();
@@ -22,10 +24,20 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
+        ArrayList<Long> arr=new ArrayList<Long>();
+        arr.add((long) 0);
+        arr.add((long) 1);
+        int k=0;
+        for(int i=2;i<6*m+2;i++){
+            arr.add((arr.get(i - 1)+(arr.get(i - 2)))%m);
+            k++;
+            if(arr.get(i)==1 && arr.get(i-1)==0)
+                break;
+        }
+        return arr.get((int) (n%k));
         //решение практически невозможно найти интуитивно
         //вам потребуется дополнительный поиск информации
         //см. период Пизано
-        return 0L;
     }
 
 
