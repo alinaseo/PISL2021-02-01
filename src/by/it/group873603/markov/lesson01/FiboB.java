3b41df2b760378e106267280d939dfab30952ea3
Alexey <alepaca28@gmail.com>
2021-04-18 18:40:16 +0300

    Lesson1(A,B,C)

---
+++ src/by/it/group873603/markov/lesson01/FiboB.java
@@ -0,0 +1,38 @@
+package by.it.group873603.markov.lesson01;
+
+import java.math.BigInteger;
+
+/*
+ * Вам необходимо выполнить способ вычисления чисел Фибоначчи с вспомогательным массивом
+ * без ограничений на размер результата (BigInteger)
+ */
+
+public class FiboB {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+
+        //вычисление чисел простым быстрым методом
+        FiboB fibo = new FiboB();
+        int n = 55555;
+        System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
+    }
+
+    BigInteger fastB(Integer n) {
+        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
+        BigInteger[] fab=new BigInteger[n+1];
+        fab[0]=BigInteger.ZERO;
+        fab[1]=BigInteger.ONE;
+        for (int i=2;i<=n;i++){
+            fab[i]=fab[i-1].add(fab[i-2]);
+        }
+        return fab[n];
+    }
+
+}
+
