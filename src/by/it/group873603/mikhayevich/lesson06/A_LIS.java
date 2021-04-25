76439348f1d17924379187ec132260013e0438f9
Dexar <dima.mikhayevich@mail.ru>
2021-04-26 00:49:57 +0300

    Lesson 6-8 (A B)

--- src/by/it/group873603/mikhayevich/lesson06/A_LIS.java
+++ src/by/it/group873603/mikhayevich/lesson06/A_LIS.java
@@ -1,4 +1,4 @@
-package by.it.a_khmelev.lesson06;
+package by.it.mikhayevich.lesson06;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
--- src/by/it/group873603/mikhayevich/lesson08/A_Knapsack.java
+++ src/by/it/group873603/mikhayevich/lesson08/A_Knapsack.java
@@ -1,4 +1,4 @@
-package by.it.a_khmelev.lesson08;
+package by.it.mikhayevich.lesson08;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
